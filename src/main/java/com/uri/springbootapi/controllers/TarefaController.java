0fa94fc3b1f58d0ce34bfbdb903b99ab92bcc60b
João Vitor Zanardo <100529680+joaovitorzanardo@users.noreply.github.com>
2024-05-21 21:48:43 -0300

    Documentação

--- src/main/java/com/uri/springbootapi/controllers/TarefaController.java
+++ src/main/java/com/uri/springbootapi/controllers/TarefaController.java
@@ -4,6 +4,10 @@ import com.uri.springbootapi.dtos.SucessResponse;
 import com.uri.springbootapi.dtos.TarefaDTO;
 import com.uri.springbootapi.entities.Tarefa;
 import com.uri.springbootapi.services.TarefaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,12 +18,17 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "/task")
+@Tag(name = "API Tarefa")
 public class TarefaController {
 
     @Autowired
     TarefaService tarefaService;
 
     @PostMapping
+    @Operation(summary = "Criar Tarefa", description = "Salvar a tarefa no banco de dados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Tarefa criada com sucesso!"),
+    })
     public ResponseEntity<SucessResponse> criarTarefa(@RequestBody @Valid TarefaDTO tarefaDTO) {
         Tarefa tarefa = tarefaService.criarTarefa(tarefaDTO);
         SucessResponse sucessResponse = SucessResponse.builder()
@@ -31,16 +40,23 @@ public class TarefaController {
     }
 
     @GetMapping
+    @Operation(summary = "Buscar Tarefas", description = "Buscar todas as tarefas salvas no banco de dados")
     public List<Tarefa> getTarefas() {
         return tarefaService.getTarefas();
     }
 
     @GetMapping(path = "/{tarefaId}")
+    @Operation(summary = "Buscar Tarefas por Id", description = "Busca uma tarefa pelo seu respectivo id")
     public Tarefa getTarefaById(@PathVariable Long tarefaId) {
         return tarefaService.getTarefaById(tarefaId);
     }
 
     @PutMapping(path = "/{tarefaId}")
+    @Operation(summary = "Editar Tarefa", description = "Alterar as informações da tarefa")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Tarefa alterada com sucesso!"),
+            @ApiResponse(responseCode = "410", description = "Tarefa não encontrada.")
+    })
     public ResponseEntity<SucessResponse> editarTarefa(@PathVariable Long tarefaId, @RequestBody @Valid TarefaDTO tarefaDTO) {
         tarefaDTO.setId(tarefaId);
         Tarefa tarefa = tarefaService.editarTarefa(tarefaDTO);
@@ -53,6 +69,11 @@ public class TarefaController {
     }
 
     @DeleteMapping(path = "/{tarefaId}")
+    @Operation(summary = "Excluir Tarefa", description = "Realiza a exclusão de uma tarefa")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Tarefa excluida com sucesso!"),
+            @ApiResponse(responseCode = "410", description = "Tarefa não encontrada.")
+    })
     public ResponseEntity<SucessResponse> excluirTarefa(@PathVariable Long tarefaId) {
         tarefaService.excluirTarefa(tarefaId);
         SucessResponse sucessResponse = SucessResponse.builder()
@@ -62,6 +83,5 @@ public class TarefaController {
                 .build();
         return ResponseEntity.status(sucessResponse.getHttpStatus()).body(sucessResponse);
     }
-
 
 }
